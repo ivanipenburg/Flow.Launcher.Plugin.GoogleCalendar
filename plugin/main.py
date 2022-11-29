2c1d08e45521b791f163b7e4a23f5dd83ad17e34
Ilja van Ipenburg <ivanipenburg@gmail.com>
2022-11-29 22:23:53 +0100

    added user interface for adding events

--- plugin/main.py
+++ plugin/main.py
@@ -5,7 +5,6 @@ from datetime import datetime
 
 from ctparse import ctparse
 from flowlauncher import FlowLauncher
-
 from templates import *
 
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
@@ -15,31 +14,34 @@ sys.path.append(os.path.join(parent_folder_path, "lib", "plugin"))
 
 
 class GoogleCalendar(FlowLauncher):
-    messages_queue = []
-
-    def sendNormalMessage(self, title, subtitle):
-        message = copy.deepcopy(RESULT_TEMPLATE)
-        message["Title"] = title
-        message["SubTitle"] = subtitle
-
-        self.messages_queue.append(message)
+    def show_result(self, title, subtitle):
+        return [
+            {
+                "Title": title,
+                "SubTitle": subtitle,
+                "IcoPath": "Images/app.png",
+            }
+        ]
 
     def query(self, query):
-        print(query)
+        if query == "":
+            return self.show_result("No event name specified yet", "Please provide a name for your event")
+
         try:
             ts = datetime.now()
             parse = ctparse(query, ts=ts)
+            if parse is None:
+                return self.show_result("No date and time specified yet", "Please specify a date")
+
             start_dt = parse.resolution.start.dt
             end_dt = parse.resolution.end.dt
 
             event_name = query[:parse.resolution.mstart - 1]
 
-            self.sendNormalMessage(
-                f"Creating event '{event_name}'", f"from {start_dt} to {end_dt}")
+            return self.show_result(f"Creating event '{event_name}'", f"from {start_dt} to {end_dt}")
+            
         except:
-            self.sendNormalMessage("Error", "Something went wrong")
-
-        return self.messages_queue
+            return self.show_result("Error", "Something went wrong")
 
 
 if __name__ == "__main__":
