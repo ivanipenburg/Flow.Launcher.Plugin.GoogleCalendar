4581d70c37cf7e0c2c648f3a8f4fcf84fa6034bf
Ilja van Ipenburg <ivanipenburg@gmail.com>
2022-12-05 13:16:15 +0100

    changed plugin root

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import sys
 
-parent_folder_path = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(parent_folder_path)
-sys.path.append(os.path.join(parent_folder_path, "lib"))
-sys.path.append(os.path.join(parent_folder_path, "plugin"))
+# parent_folder_path = os.path.abspath(os.path.dirname(__file__))
+# sys.path.append(parent_folder_path)
+# sys.path.append(os.path.join(parent_folder_path, "lib"))
+# sys.path.append(os.path.join(parent_folder_path, "plugin"))
+
+plugin_root = sys.argv[0]
+os.chdir(os.path.dirname(os.path.abspath(plugin_root)))
 
 from plugin.main import GoogleCalendar
 
