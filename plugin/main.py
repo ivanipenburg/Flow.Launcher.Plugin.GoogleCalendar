7fb04f4ebd57f2687464875c38a89e600544b3c7
Ilja van Ipenburg <ivanipenburg@gmail.com>
2022-12-05 13:37:35 +0100

    fixed local imports

--- plugin/main.py
+++ plugin/main.py
@@ -3,11 +3,12 @@ import os
 import webbrowser
 from datetime import datetime
 
-from credentials import credentials_exist, get_credentials
 from ctparse import ctparse
 from flowlauncher import FlowLauncher
 from googleapiclient import discovery
-from templates import ACTION_RESULT, EVENT, SHOW_RESULT
+
+from .credentials import credentials_exist, get_credentials
+from .templates import ACTION_RESULT, EVENT, SHOW_RESULT
 
 SETUP_URL = "https://github.com/ivanipenburg/Flow.Launcher.Plugin.GoogleCalendar#setup"
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
