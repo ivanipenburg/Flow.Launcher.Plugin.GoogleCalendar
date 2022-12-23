ee0576fd524d5d21dcd15f408e1f2b024fea686e
Ilja van Ipenburg <ivanipenburg@gmail.com>
2022-12-23 14:43:45 +0100

    code for displaying upcoming events

--- plugin/main.py
+++ plugin/main.py
@@ -4,46 +4,41 @@ from datetime import datetime
 from apiclient import discovery
 from credentials import credentials_exist, get_credentials
 from ctparse import ctparse
+from events import get_event_times, get_sorted_events
 from flox import Flox
+from icons import date_to_glyph_id
 from templates import EVENT
 
 SETUP_URL = "https://github.com/ivanipenburg/Flow.Launcher.Plugin.GoogleCalendar#setup"
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%S"
+NUM_UPCOMING_EVENTS = 20
 
 
 class GoogleCalendar(Flox):
     def __init__(self):
-        self.upcoming_events = []
+        super().__init__()
+        self.font_family = "#Date-Icons"
 
 
-    def show_result(self, title, subtitle):
+    def show_result(self, title, subtitle, score=99, glyph_id="", method=None, params=None):
         self.add_item(
             title=title,
             subtitle=subtitle,
-        )
-
-    def action_result(self, title, subtitle, action, params):
-        self.add_item(
-            title=title,
-            subtitle=subtitle,
-            method=action,
+            score=score,
+            glyph=glyph_id,
+            method=method,
             parameters=params,
         )
-
     
-    def open_webpage(self, url):
-        webbrowser.open(url)
-
 
     def query(self, query):
         if not credentials_exist():
             return self.action_result("It seems like you have not set up your credentials yet", "Press Enter to open the setup page", "open_webpage", [SETUP_URL])
 
-        if self.upcoming_events == []:
-            self.display_current_events()
+        self.display_current_events()
 
         if query == "":
-            self.show_result("No event name specified yet", "Please provide a name for your event")
+            return self.show_result("No event name specified yet", "Please provide a name for your event")
 
         try:
             ts = datetime.now()
@@ -60,9 +55,13 @@ class GoogleCalendar(Flox):
 
             event_name = query[:parse.resolution.mstart - 1]
 
-            return self.action_result(f"Creating event '{event_name}'", f"from {start_dt} to {end_dt}", "create_event", [event_name, start_dt_string, end_dt_string])
+            return self.show_result(f"Creating event '{event_name}'", f"from {start_dt} to {end_dt}", method="create_event", params=[event_name, start_dt_string, end_dt_string])
         except Exception as e:
             return self.show_result("Error", str(e))
+
+
+    def open_webpage(self, url):
+        webbrowser.open(url)
 
 
     def create_event(self, event_name, start_dt, end_dt):
@@ -91,14 +90,26 @@ class GoogleCalendar(Flox):
     def display_current_events(self):
         credentials = get_credentials()
 
+        self.show_result("Upcoming events", "", score=40)
+        
         try:
             service = discovery.build('calendar', 'v3', credentials=credentials)
 
-            events = service.events().list(calendarId='primary', timeMin=datetime.utcnow().isoformat() + 'Z', maxResults=10, singleEvents=True, orderBy='startTime').execute()
+            all_events = get_sorted_events(service, NUM_UPCOMING_EVENTS)
 
-            for event in events['items']:
-                start = event['start'].get('dateTime', event['start'].get('date'))
-                self.show_result(event['summary'], start)
+            for i, event in enumerate(all_events):
+                start_dt, _, start_time, end_time = get_event_times(event, TIME_FORMAT)
+
+                location = event['location'] if 'location' in event else None
+
+                subtitle = ""
+                if start_time is not None and end_time is not None:
+                    subtitle += f"{start_time} - {end_time}"
+                if location is not None:
+                    subtitle += f" @ {location}"
+
+                date_glyph_id = date_to_glyph_id(start_dt)                
+                self.show_result(event['summary'], subtitle, score=10-i, glyph_id=date_glyph_id, method="open_webpage", params=[event['htmlLink']])
         except Exception as e:
             self.show_result('Could not load upcoming events', e)
 
